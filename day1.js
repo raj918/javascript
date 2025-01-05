bd907ce6115d38cc9fefbf5ccb335aa0ae6b95a1
vaibhav <115678028+raj918@users.noreply.github.com>
2025-01-05 07:10:02 -0800

    Add files via upload

---
+++ day1.js
@@ -0,0 +1,43 @@
+//Task 1
+var a = 5
+
+console.log(a);
+
+//Task2
+
+let b = "coco"
+console.log(b);
+
+//Task3
+const c = true
+
+console.log(c);
+
+//Task4
+
+let ab = 5
+let cd = "coco"
+let ef = true
+let gh = {
+    name  : "coco",
+    age : 4
+}
+let ij = [1,2,3,4,5]
+
+console.log(typeof (ab), ab);
+console.log(typeof (cd), cd) ;
+console.log(typeof (ef), ef);
+console.log(typeof  (gh), gh);
+console.log(typeof (ij), ij);
+
+//Task5
+
+let abc = 5
+console.log(abc);
+abc = "coco"
+console.log(abc);
+
+const bcd = 5
+console.log(bcd);
+// bcd = 20
+// console.log(bcd); reassigning const is not possible as it gives a type error
---
+++ day2.js
@@ -0,0 +1,28 @@
+let a = 5
+let b = 5
+ let sum = a + b
+console.log(sum)
+
+
+let n1 = 5
+let n2 = 10
+let sub = n1 - n2
+console.log(sub)
+
+let m1 = 5
+let m2 = 10
+let multi = m1 * m2
+console.log(multi)
+
+let d1 = 9
+let d2 = 3
+let div = d1 % d2
+console.log(div)
+
+let abc = 5
+abc += 20
+console.log(abc)
+
+let bcd = 30
+bcd -= 20
+console.log(bcd)
